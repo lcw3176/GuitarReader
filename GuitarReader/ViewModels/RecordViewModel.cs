f2da62e1bee28450fe5be1d3d7ad6dedd2c10fe6
joebrooks <lcw3176@naver.com>
2021-09-09 17:27:54 +0900

    레코드 커맨드 연결

--- GuitarReader/ViewModels/RecordViewModel.cs
+++ GuitarReader/ViewModels/RecordViewModel.cs
@@ -1,6 +1,36 @@
-﻿namespace GuitarReader.ViewModels
+﻿using GuitarReader.Command;
+using GuitarReader.Services;
+using System;
+using System.Windows.Input;
+
+namespace GuitarReader.ViewModels
 {
     class RecordViewModel : BaseViewModel
     {
+        public ICommand RecordCommand { get; set; }
+        private RecordService recordService = new RecordService();
+        private bool isRun = false;
+
+        public RecordViewModel()
+        {
+            RecordCommand = new RelayCommand(RecordExecuteMethod);
+        }
+
+
+        private void RecordExecuteMethod(object gridSheet)
+        {
+            if (!isRun)
+            {
+                recordService.StartRecord(gridSheet);
+                isRun = true;
+            }
+
+            else
+            {
+                recordService.StopRecord();
+                isRun = false;
+            }
+            
+        }
     }
 }
