5f540e297219d0c0f6e58546f1511990c8afea6e
joebrooks <lcw3176@naver.com>
2021-09-09 17:27:17 +0900

    커맨드 추가

---
+++ GuitarReader/Command/RelayCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace GuitarReader.Command
+{
+    class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action<object> executeMethod;
+
+        public RelayCommand(Action<object> executeMethod)
+        {
+            this.executeMethod = executeMethod;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            executeMethod(parameter);
+        }
+    }
+}
