8ab5c4a67348f86aa3cbe26a77e1dc403865027c
lcw3176 <lcw3176@naver.com>
2021-09-11 15:39:02 +0900

    SheetService 추가

---
+++ GuitarReader/Services/SheetService.cs
@@ -0,0 +1,28 @@
+﻿using GuitarReader.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarReader.Services
+{
+    class SheetService
+    {
+
+        private void WriteSheet(Sheet sheet)
+        {
+
+        }
+
+        private List<Sheet> ReadAllSheets()
+        {
+            return null;
+        }
+
+        private Sheet ReadSheetByName(string sheetName)
+        {
+            return null;
+        }
+    }
+}
