dbdad1f4b3184730a73683d41df69f2a1ee90202
lcw3176 <lcw3176@naver.com>
2021-09-11 15:38:21 +0900

    Sheet 추가

---
+++ GuitarReader/Models/Sheet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GuitarReader.Models
+{
+    class Sheet
+    {
+        public List<Note> notes = new List<Note>();
+        public string name;
+        public DateTime created;
+        public DateTime lastModified;
+    }
+}
