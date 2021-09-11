76847525b25c5477c33c4e5e8f14ff8c24220757
lcw3176 <lcw3176@naver.com>
2021-09-11 14:56:55 +0900

    테스트 코드 추가

--- GuitarReader/ViewModels/ParseViewModel.cs
+++ GuitarReader/ViewModels/ParseViewModel.cs
@@ -3,11 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace GuitarReader.ViewModels
 {
     class ParseViewModel : BaseViewModel
     {
+        private int rowPosition;
+        public int RowPosition
+        {
+            get { return rowPosition; }
+            set
+            {
+                rowPosition = value;
+                OnPropertyChanged("RowPosition");
+            }
+        }
 
+        private int columnPosition;
+        public int ColumnPosition
+        {
+            get { return columnPosition; }
+            set
+            {
+                columnPosition = value;
+                OnPropertyChanged("ColumnPosition");
+            }
+        }
+
+        
+
+        public ParseViewModel()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(0.5);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            RowPosition = random.Next(0, 6);
+            ColumnPosition = random.Next(0, 10);
+        }
     }
 }
