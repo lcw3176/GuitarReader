e2b1847c782161784459b88a121d5bf2dac33d8d
joebrooks <lcw3176@naver.com>
2021-09-12 12:22:58 +0900

    id, 주석추가

--- GuitarReader/Models/Sheet.cs
+++ GuitarReader/Models/Sheet.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 
 namespace GuitarReader.Models
 {
-    class Sheet
+    public class Sheet
     {
-        public List<Note> notes = new List<Note>();
-        public string name;
-        public DateTime created;
-        public DateTime lastModified;
+        /// <summary>
+        /// 인덱스 primary key, auto increment
+        /// </summary>
+        public int id { get; set; }
+        public List<Note> notes { get; set; }
+
+        /// <summary>
+        /// 악보 이름
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// 처음 만든 날짜
+        /// </summary>
+        public DateTime created { get; set; }
+
+        /// <summary>
+        /// 마지막 수정 날짜
+        /// </summary>
+        public DateTime lastModified { get; set; }
     }
 }
