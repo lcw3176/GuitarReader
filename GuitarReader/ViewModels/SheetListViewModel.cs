aa88ad3869a9ceb253d44899a65f4a1151a74344
joebrooks <lcw3176@naver.com>
2021-09-12 12:22:28 +0900

    sheetListViewModel 추가

---
+++ GuitarReader/ViewModels/SheetListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GuitarReader.Models;
+using System.Collections.ObjectModel;
+
+namespace GuitarReader.ViewModels
+{
+    class SheetListViewModel : BaseViewModel
+    {
+        public ObservableCollection<Sheet> SheetsCollection { get; set; } = new ObservableCollection<Sheet>();
+
+        public SheetListViewModel()
+        {
+
+        }
+    }
+}
