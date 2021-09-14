bfe50a78bbc68de755f4d5cb3afed48b91fb7029
joebrooks <lcw3176@naver.com>
2021-09-14 14:36:17 +0900

    세이브 커맨드 추가

--- GuitarReader/ViewModels/RecordViewModel.cs
+++ GuitarReader/ViewModels/RecordViewModel.cs
@@ -8,15 +8,29 @@ namespace GuitarReader.ViewModels
     class RecordViewModel : BaseViewModel
     {
         public ICommand RecordCommand { get; set; }
+        public ICommand SaveCommand { get; set; }
         private RecordService recordService = new RecordService();
         private bool isRun = false;
 
         public RecordViewModel()
         {
             RecordCommand = new RelayCommand(RecordExecuteMethod);
+            SaveCommand = new RelayCommand(SaveExeucteMethod);
         }
 
+        /// <summary>
+        /// 악보 저장
+        /// </summary>
+        /// <param name="obj"></param>
+        private void SaveExeucteMethod(object obj)
+        {
 
+        }
+
+        /// <summary>
+        /// 녹음 시작
+        /// </summary>
+        /// <param name="gridSheet">그리드 오브젝트</param>
         private void RecordExecuteMethod(object gridSheet)
         {
             if (!isRun)
