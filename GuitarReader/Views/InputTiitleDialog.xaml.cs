cdcd4b49437e380960489ec704254a5bd4167039
lcw3176 <lcw3176@naver.com>
2021-09-14 16:53:23 +0900

    타이틀 다이얼로그 생성

---
+++ GuitarReader/Views/InputTiitleDialog.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GuitarReader.Views
+{
+    /// <summary>
+    /// InputTiitleDialog.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class InputTiitleDialog : Window
+    {
+        public InputTiitleDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void confirm_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}
