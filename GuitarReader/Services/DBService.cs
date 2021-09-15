d899ca5b9f7bd46bd04ec3642ba841a58c5ff1e1
joebrooks <lcw3176@naver.com>
2021-09-15 23:38:24 +0900

    싱글톤 서비스로 변경

--- GuitarReader/Services/DBService.cs
+++ GuitarReader/Services/DBService.cs
@@ -1,175 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
-using System.Reflection;
-using System.Text;
 
 namespace GuitarReader.Services
 {
-    class DBService<T>
+    class DBService
     {
         private string location = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "guitarReader.db");
-        private string connStr;
+        private static DBService instance;
+        private static SQLiteConnection conn;
 
-        public DBService()
+        public static DBService GetInstace()
         {
-            connStr = "Data Source=" + location;
+            if(instance == null)
+            {
+                instance = new DBService();
+            }
 
+            return instance;
+        }
+
+        private DBService()
+        {
+            string connStr = "Data Source=" + location;
+            conn = new SQLiteConnection(connStr);
+            conn.Open();
+            
             if (!IsExist())
             {
                 InitTables();
             }
         }
 
-        public bool IsExist()
+        public SQLiteConnection GetConnection()
+        {
+            return conn;
+        }
+
+        public void CloseConnection()
+        {
+            conn.Close();
+            conn.Dispose();
+        }
+
+        private bool IsExist()
         {
             return new FileInfo(location).Exists;
         }
 
-        public void InitTables()
+
+        private void InitTables()
         {
-            using (SQLiteConnection conn = new SQLiteConnection(connStr))
-            {
-                conn.Open();
-                string sheetDDL = "CREATE TABLE SHEET(" +
-                    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    "name CHAR(100) not null," +
-                    "created CHAR(100) not null," +
-                    "lastModified CHAR(100) not null);";
-                var cmd = new SQLiteCommand(sheetDDL, conn);
-                cmd.ExecuteNonQuery();
-
-                string noteDDL = "CREATE TABLE NOTE(" +
-                    "id INTEGER," +
-                    "stringPos INTEGER not null," +
-                    "fretPos INTEGER not null," +
-                    "beatLen INTEGER not null," +
-                    "FOREIGN KEY(id)" +
-                    "REFERENCES SHEET(id)" +
-                    "ON DELETE CASCADE " +
-                    "ON UPDATE CASCADE)";
-
-                cmd = new SQLiteCommand(noteDDL, conn);
-                cmd.ExecuteNonQuery();
-            }
-        }
-
-        public object Write(string tableName, T value)
-        {
-            Type type = typeof(T);
-            PropertyInfo[] propertyInfos = type.GetProperties();
-
-
-            StringBuilder sb = new StringBuilder();
-            sb.Append("INSERT INTO ");
-            sb.Append(tableName);
-            sb.Append("(");
-
-            foreach (PropertyInfo i in propertyInfos)
-            {
-                if (i.Name.ToLower().Contains("key"))
-                {
-                    continue;
-                }
-                sb.Append(i.Name);
-                sb.Append(",");
-            }
-
-            sb.Remove(sb.Length - 1, 1);
-            sb.Append(")");
-
-            sb.Append(" VALUES");
-            sb.Append("(");
-
-           
-            foreach (PropertyInfo i in propertyInfos)
-            {
-                if (i.Name.ToLower().Contains("key"))
-                {
-                    continue;
-                }
-                sb.Append("'");
-                sb.Append(i.GetValue(value));
-                sb.Append("'");
-                sb.Append(",");
-            }
-
-            sb.Remove(sb.Length - 1, 1);
-            sb.Append(")");
-
-            Console.WriteLine(sb.ToString());
-            try
-            {
-                using (SQLiteConnection conn = new SQLiteConnection(connStr))
-                {
-                    conn.Open();
-                    SQLiteCommand cmd = new SQLiteCommand(sb.ToString(), conn);
-                    cmd.ExecuteNonQuery();
-                    cmd.Dispose();
-
-                }
-
-                return true;
-            }
-
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex);
-                return false;
-            }
-        }
-
-        public List<T> Read(string tableName, params string[] readElements)
-        {
-            List<T> lst = new List<T>();
-            StringBuilder query = new StringBuilder();
-            query.Append("SELECT ");
-
-            foreach (string i in readElements)
-            {
-                query.Append(i);
-                query.Append(",");
-            }
-
-            query.Remove(query.Length - 1, 1);
-
-            query.Append(" FROM ");
-            query.Append(tableName);
-
-            Type type = typeof(T);
-            PropertyInfo[] propertyInfos = type.GetProperties();
-
-            using (SQLiteConnection conn = new SQLiteConnection(connStr))
-            {
-                conn.Open();
-                SQLiteCommand cmd = new SQLiteCommand(query.ToString(), conn);
-                SQLiteDataReader reader = cmd.ExecuteReader();
-                
-
-                while (reader.Read())
-                {
-                    object obj = Activator.CreateInstance(type);
-
-                    foreach (PropertyInfo i in propertyInfos)
-                    {
-                        if (i.Name.ToLower().Contains("key"))
-                        {
-                            continue;
-                        }
-
-                        PropertyInfo propInfo = type.GetProperty(i.Name);
-                        propInfo.SetValue(obj, reader[i.Name]);
-                    }
-
-                    lst.Add((T)obj);
-                }
-
-                cmd.Dispose();
-                reader.Close();
-            }
-
-            return lst;
+            string sheetDDL = "CREATE TABLE SHEET(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name CHAR(100) UNIQUE NOT NULL," +
+                "created CHAR(100) NOT NULL," +
+                "lastModified CHAR(100) NOT NULL);";
+            var cmd = new SQLiteCommand(sheetDDL, conn);
+            cmd.ExecuteNonQuery();
+            
+            string noteDDL = "CREATE TABLE NOTE(" +
+                "id INTEGER," +
+                "stringPos INTEGER NOT NULL," +
+                "fretPos INTEGER NOT NULL," +
+                "beatLen INTEGER NOT NULL," +
+                "FOREIGN KEY(id)" +
+                "REFERENCES SHEET(id)" +
+                "ON DELETE CASCADE " +
+                "ON UPDATE CASCADE)";
+            
+            cmd = new SQLiteCommand(noteDDL, conn);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
         }
 
     }
