a585a790a7dd7eec4365a5e0b5998f0e1e789574
joebrooks <lcw3176@naver.com>
2021-09-15 23:38:32 +0900

    NoteService 추가

---
+++ GuitarReader/Services/NoteService.cs
@@ -0,0 +1,23 @@
+﻿using GuitarReader.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuitarReader.Services
+{
+    class NoteService
+    {
+        public void Insert(Note note)
+        {
+
+        }
+
+        public Note ReadByName(string name)
+        {
+            return null;
+        }
+            
+    }
+}
