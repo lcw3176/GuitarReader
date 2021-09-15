075b17fff99122dd414d51d4263a23b35e49750c
joebrooks <lcw3176@naver.com>
2021-09-15 23:37:24 +0900

    함수 내용 기술

--- GuitarReader/Services/SheetService.cs
+++ GuitarReader/Services/SheetService.cs
@@ -1,28 +1,73 @@
 ﻿using GuitarReader.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace GuitarReader.Services
 {
     class SheetService
     {
+        private DBService dBService = DBService.GetInstace();
 
-        private void WriteSheet(Sheet sheet)
+        public void Insert(Sheet sheet)
         {
+            SQLiteConnection conn = dBService.GetConnection();
+            string cmd = string.Format("INSERT INTO SHEET(name, created, lastModified) VALUES('{0}', '{1}', '{2}')", sheet.name, sheet.created, sheet.lastModified);
 
+            using (SQLiteCommand command = new SQLiteCommand(cmd, conn))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+        
+
+        public List<Sheet> ReadAll()
+        {
+            SQLiteConnection conn = dBService.GetConnection();
+            string cmd = "SELECT * FROM SHEET";
+            List<Sheet> lst = new List<Sheet>();
+
+            using (SQLiteCommand command = new SQLiteCommand(cmd, conn))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Sheet temp = new Sheet();
+                        temp.id = int.Parse(reader[nameof(temp.id)].ToString());
+                        temp.created = reader[nameof(temp.created)].ToString();
+                        temp.lastModified = reader[nameof(temp.lastModified)].ToString();
+                        temp.name = reader[nameof(temp.name)].ToString();
+
+                        lst.Add(temp);
+                    }
+                }
+
+            }
+
+            return lst;
         }
 
-        private List<Sheet> ReadAllSheets()
+        public Sheet ReadMostRecent()
         {
-            return null;
-        }
+            SQLiteConnection conn = dBService.GetConnection();
+            string cmd = "SELECT * FROM SHEET ORDER BY id DESC LIMIT 1";
+            Sheet temp = new Sheet();
 
-        private Sheet ReadSheetByName(string sheetName)
-        {
-            return null;
+            using (SQLiteCommand command = new SQLiteCommand(cmd, conn))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        temp.id = int.Parse(reader[nameof(temp.id)].ToString());
+                        temp.created = reader[nameof(temp.created)].ToString();
+                        temp.lastModified = reader[nameof(temp.lastModified)].ToString();
+                        temp.name = reader[nameof(temp.name)].ToString();
+                    }
+                }
+            }
+
+            return temp;
         }
     }
 }
