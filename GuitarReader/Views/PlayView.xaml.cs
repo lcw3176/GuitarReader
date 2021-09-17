6e526ad94ff6450124fc968a78aeff921a703654
lcw3176 <lcw3176@naver.com>
2021-09-17 16:39:29 +0900

    PlayView 추가

---
+++ GuitarReader/Views/PlayView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GuitarReader.Views
+{
+    /// <summary>
+    /// PlayView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class PlayView : UserControl
+    {
+        public PlayView()
+        {
+            InitializeComponent();
+        }
+    }
+}
