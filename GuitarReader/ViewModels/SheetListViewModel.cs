f3cd84dba1ccf781285cdf986df75652f025e79d
lcw3176 <lcw3176@naver.com>
2021-09-17 16:40:47 +0900

    LoadSheetCommand 추가

--- GuitarReader/ViewModels/SheetListViewModel.cs
+++ GuitarReader/ViewModels/SheetListViewModel.cs
@@ -1,17 +1,37 @@
-﻿using GuitarReader.Models;
+﻿using GuitarReader.Command;
+using GuitarReader.Models;
 using GuitarReader.Services;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows.Input;
+using Toub.Sound.Midi;
 
 namespace GuitarReader.ViewModels
 {
     class SheetListViewModel : BaseViewModel
     {
         public ObservableCollection<Sheet> SheetsCollection { get; set; } = new ObservableCollection<Sheet>();
+        public ICommand LoadSheetCommand { get; private set; }
+
+        private SheetService sheetService = new SheetService();
 
         public SheetListViewModel()
         {
-            SheetService sheetService = new SheetService();
-            sheetService.ReadAll().ForEach(i => SheetsCollection.Add(i));
+            LoadSheetCommand = new RelayCommand(LoadSheetExecuteMethod);
+
+            sheetService.ReadAll().ForEach((i) =>
+            {
+                i.LoadSheetCommand = this.LoadSheetCommand;
+                SheetsCollection.Add(i);
+            });
+        }
+
+        private void LoadSheetExecuteMethod(object obj)
+        {
+            Sheet sheet = sheetService.ReadByName(obj.ToString());
+            EnqueuePlayList(sheet.id);
         }
     }
 }
