65aa0172d2fc4c5e3fb273559183972112b74112
joebrooks <lcw3176@naver.com>
2021-09-18 18:28:04 +0900

    커맨드 추가 연결

--- GuitarReader/ViewModels/SheetListViewModel.cs
+++ GuitarReader/ViewModels/SheetListViewModel.cs
@@ -9,25 +9,39 @@ namespace GuitarReader.ViewModels
     class SheetListViewModel : BaseViewModel
     {
         public ObservableCollection<Sheet> SheetsCollection { get; set; } = new ObservableCollection<Sheet>();
-        public ICommand LoadSheetCommand { get; private set; }
+        public ICommand PlaySheetCommand { get; private set; }
+        public ICommand EditSheetCommand { get; private set; }
 
         private SheetService sheetService = new SheetService();
 
         public SheetListViewModel()
         {
-            LoadSheetCommand = new RelayCommand(LoadSheetExecuteMethod);
-
+            PlaySheetCommand = new RelayCommand(PlaySheetExecuteMethod);
+            EditSheetCommand = new RelayCommand(EditSheetExecuteMethod);
             sheetService.ReadAll().ForEach((i) =>
             {
-                i.LoadSheetCommand = this.LoadSheetCommand;
+                i.PlaySheetCommand = this.PlaySheetCommand;
+                i.EditSheetCommand = this.EditSheetCommand;
                 SheetsCollection.Add(i);
             });
         }
 
-        private void LoadSheetExecuteMethod(object obj)
+        /// <summary>
+        /// 클릭 시 악보 재생 커맨드
+        /// </summary>
+        /// <param name="sheetName">악보 이름</param>
+        private void PlaySheetExecuteMethod(object sheetName)
         {
-            Sheet sheet = sheetService.ReadByName(obj.ToString());
-            EnqueuePlayList(sheet.id);
+            Sheet sheet = sheetService.ReadByName(sheetName.ToString());
+        }
+
+        /// <summary>
+        /// 클릭 시 악보 편집 커맨드
+        /// </summary>
+        /// <param name="sheetName">악보 이름</param>
+        private void EditSheetExecuteMethod(object sheetName)
+        {
+            
         }
     }
 }
