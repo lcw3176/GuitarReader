d9979447bb1fb15fb267f43bb23c8a06725fd647
joebrooks <lcw3176@naver.com>
2021-09-19 20:16:04 +0900

    커맨드 리팩토링

--- GuitarReader/ViewModels/PlayViewModel.cs
+++ GuitarReader/ViewModels/PlayViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using GuitarReader.Command;
 using GuitarReader.Services;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace GuitarReader.ViewModels
@@ -7,33 +8,71 @@ namespace GuitarReader.ViewModels
     class PlayViewModel : BaseViewModel
     {
         public ICommand PlayCommand { get; private set; }
+        public ICommand PauseCommand { get; private set; }
         public ICommand StopCommand { get; private set; }
-
-        private PlayService playService = new PlayService();
         private bool isRun = false;
 
-        
-        public PlayViewModel()
+        public bool IsRun
+        {
+            get { return isRun; }
+            set
+            {
+                isRun = value;
+                OnPropertyChanged("IsRun");
+            }
+        }
+
+        private PlayService playService = PlayService.GetInstacne();
+        private AnimService animService;
+        private int playId;
+
+        public PlayViewModel(Grid _gridSheet, int _playId)
         {
             PlayCommand = new RelayCommand(PlayExecuteMethod);
+            PauseCommand = new RelayCommand(PauseExecuteMethod);
             StopCommand = new RelayCommand(StopExecuteMethod);
+            animService = new AnimService(_gridSheet);
             
+            playService.beatPlayEvent += (stringPos, fretPos) => animService.Start(stringPos, fretPos);
+            playService.beatEndEvent += () => { IsRun = false; };
+
+            playId = _playId;
         }
 
 
         /// <summary>
         /// 재생 버튼 커맨드
         /// </summary>
-        /// <param name="grid">view 그리드 오브젝트</param>
-        private void PlayExecuteMethod(object grid)
+        /// <param name="obj"></param>
+        private void PlayExecuteMethod(object obj)
         {
-            isRun = true;
-            playService.Play(grid, playId);
+            IsRun = true;
+            playService.Play(playId);
+            animService.Resume();
         }
 
+
+        /// <summary>
+        /// 일시 정지 커맨드
+        /// </summary>
+        /// <param name="obj"></param>
+        private void PauseExecuteMethod(object obj)
+        {
+            IsRun = false;
+            playService.Pause();
+            animService.Pause();
+        }
+
+
+        /// <summary>
+        /// 정지 커맨드
+        /// </summary>
+        /// <param name="obj"></param>
         private void StopExecuteMethod(object obj)
         {
-            
+            IsRun = false;
+            playService.Stop();
+            animService.Stop();
         }
     }
 }
