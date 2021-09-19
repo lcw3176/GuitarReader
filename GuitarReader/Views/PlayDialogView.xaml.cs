ffff160706fd249238bc941ea2c36fb67ce64c4a
joebrooks <lcw3176@naver.com>
2021-09-19 20:14:42 +0900

    Window Dialog 스타일로 변경

---
+++ GuitarReader/Views/PlayDialogView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using GuitarReader.Services;
+using GuitarReader.ViewModels;
+using System.Windows;
+
+namespace GuitarReader.Views
+{
+    /// <summary>
+    /// PlayView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class PlayView : Window
+    {
+        public PlayView(int playId)
+        {
+            InitializeComponent();
+            this.DataContext = new PlayViewModel(gridSheet, playId);
+            PlayService.GetInstacne().Open();
+        }
+
+        private void Window_Closed(object sender, System.EventArgs e)
+        {
+            PlayService.GetInstacne().Close();
+        }
+    }
+}
