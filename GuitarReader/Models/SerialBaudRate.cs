b3da7a29c2f510e6d70f3b81d98739353349a704
joebrooks <lcw3176@naver.com>
2021-10-29 13:16:40 +0900

    보드레이트 추가

---
+++ GuitarReader/Models/SerialBaudRate.cs
@@ -0,0 +1,14 @@
+﻿namespace GuitarReader.Models
+{
+    class SerialBaudRate
+    {
+        public int rate { get; set; }
+
+        public static int[] BaudRate { get; set; } = { 9600, 115200 };
+
+    }
+
+
+    
+
+}
