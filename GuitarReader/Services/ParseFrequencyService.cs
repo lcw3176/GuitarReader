908a4714dc987cfe582b1058451ae35d5f60a2ca
joebrooks <lcw3176@naver.com>
2021-10-29 13:16:49 +0900

    주파수 분석 추가

---
+++ GuitarReader/Services/ParseFrequencyService.cs
@@ -0,0 +1,28 @@
+﻿using GuitarReader.Models;
+using System;
+using System.Collections.Generic;
+
+namespace GuitarReader.Services
+{
+    class ParseFrequencyService
+    {
+
+        public string Parse(int hz)
+        {
+            Note note = new Note();
+            string result = string.Empty;
+            
+            foreach (string i in note.frequencyDict.Keys)
+            {
+                if(Math.Abs(note.frequencyDict[i] * 0.97 - hz) <= 7)
+                {
+                    result = i;
+                    break;
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
