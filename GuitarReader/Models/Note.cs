693f2354ff65cc0f7faef76e987adc4b0e00a56c
joebrooks <lcw3176@naver.com>
2021-10-30 18:03:26 +0900

    키값 리턴 추가

--- GuitarReader/Models/Note.cs
+++ GuitarReader/Models/Note.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace GuitarReader.Models
 {
@@ -19,7 +20,7 @@ namespace GuitarReader.Models
             set
             {
                 _stringPos = value;
-                //if(fretPos != -1)
+                //if (fretPos != -1)
                 //{
                 //    Generate();
                 //}
@@ -36,7 +37,7 @@ namespace GuitarReader.Models
             set
             {
                 _fretPos = value;
-                //if(stringPos != -1)
+                //if (stringPos != -1)
                 //{
                 //    Generate();
                 //}
@@ -51,9 +52,7 @@ namespace GuitarReader.Models
         /// <summary>
         /// 음표 문자열
         /// </summary>
-        public string CodeStr { get { return codeStr; } }
-
-        private string codeStr;
+        public string CodeStr { get { return dict.FirstOrDefault(x => x.Value == (stringPos, fretPos)).Key; } }
 
         public Dictionary<string, (int, int)> dict = new Dictionary<string, (int, int)>();
         public Dictionary<string, int> frequencyDict = new Dictionary<string, int>();
@@ -73,90 +72,94 @@ namespace GuitarReader.Models
 
         public Note()
         {
-
-            for (int i = 2; i <= 4; i++)
+            if (frequencyDict.Count == 0)
             {
-                if(i == 2)
+                for (int i = 2; i <= 4; i++)
                 {
-                    frequencyDict.Add("E" + i.ToString(), e[i]);
-                    frequencyDict.Add("F" + i.ToString(), f[i]);
-                    frequencyDict.Add("F#" + i.ToString(), fs[i]);
-                    frequencyDict.Add("G" + i.ToString(), g[i]);
-                    frequencyDict.Add("G#" + i.ToString(), gs[i]);
+                    if (i == 2)
+                    {
+                        frequencyDict.Add("E" + i.ToString(), e[i]);
+                        frequencyDict.Add("F" + i.ToString(), f[i]);
+                        frequencyDict.Add("F#" + i.ToString(), fs[i]);
+                        frequencyDict.Add("G" + i.ToString(), g[i]);
+                        frequencyDict.Add("G#" + i.ToString(), gs[i]);
 
-                    frequencyDict.Add("A" + i.ToString(), a[i]);
-                    frequencyDict.Add("A#" + i.ToString(), asharp[i]);
-                    frequencyDict.Add("B" + i.ToString(), b[i]);
+                        frequencyDict.Add("A" + i.ToString(), a[i]);
+                        frequencyDict.Add("A#" + i.ToString(), asharp[i]);
+                        frequencyDict.Add("B" + i.ToString(), b[i]);
+                    }
+
+                    else
+                    {
+                        frequencyDict.Add("C" + i.ToString(), c[i]);
+                        frequencyDict.Add("C#" + i.ToString(), cs[i]);
+                        frequencyDict.Add("D" + i.ToString(), d[i]);
+                        frequencyDict.Add("D#" + i.ToString(), ds[i]);
+                        frequencyDict.Add("E" + i.ToString(), e[i]);
+                        frequencyDict.Add("F" + i.ToString(), f[i]);
+                        frequencyDict.Add("F#" + i.ToString(), fs[i]);
+                        frequencyDict.Add("G" + i.ToString(), g[i]);
+                        frequencyDict.Add("G#" + i.ToString(), gs[i]);
+
+                        frequencyDict.Add("A" + i.ToString(), a[i]);
+                        frequencyDict.Add("A#" + i.ToString(), asharp[i]);
+                        frequencyDict.Add("B" + i.ToString(), b[i]);
+                    }
+
                 }
 
-                else
-                {
-                    frequencyDict.Add("C" + i.ToString(), c[i]);
-                    frequencyDict.Add("C#" + i.ToString(), cs[i]);
-                    frequencyDict.Add("D" + i.ToString(), d[i]);
-                    frequencyDict.Add("D#" + i.ToString(), ds[i]);
-                    frequencyDict.Add("E" + i.ToString(), e[i]);
-                    frequencyDict.Add("F" + i.ToString(), f[i]);
-                    frequencyDict.Add("F#" + i.ToString(), fs[i]);
-                    frequencyDict.Add("G" + i.ToString(), g[i]);
-                    frequencyDict.Add("G#" + i.ToString(), gs[i]);
+                dict.Add("E2", (6, 0));
+                dict.Add("F2", (6, 1));
+                dict.Add("F#2", (6, 2));
+                dict.Add("G2", (6, 3));
+                dict.Add("G#2", (6, 4));
+                //dict.Add("A2", (6, 5));
+                //dict.Add("A#2", (6, 6));
 
-                    frequencyDict.Add("A" + i.ToString(), a[i]);
-                    frequencyDict.Add("A#" + i.ToString(), asharp[i]);
-                    frequencyDict.Add("B" + i.ToString(), b[i]);
-                }
-       
+                dict.Add("A2", (5, 0));
+                dict.Add("A#2", (5, 1));
+                dict.Add("B2", (5, 2));
+                dict.Add("C3", (5, 3));
+                dict.Add("C#3", (5, 4));
+                //dict.Add("D3",(5, 5));
+                //dict.Add("D#3",(5, 6));
+
+                dict.Add("D3", (4, 0));
+                dict.Add("D#3", (4, 1));
+                dict.Add("E3", (4, 2));
+                dict.Add("F3", (4, 3));
+                dict.Add("F#3", (4, 4));
+                //dict.Add("G3",(4, 5));
+                //dict.Add("G#3",(4, 6));
+
+                dict.Add("G3", (3, 0));
+                dict.Add("G#3", (3, 1));
+                dict.Add("A3", (3, 2));
+                dict.Add("A#3", (3, 3));
+                //dict.Add("B3",(3, 4));
+                //dict.Add("C4",(3, 5));
+                //dict.Add("C#4",(3, 6));
+
+                dict.Add("B3", (2, 0));
+                dict.Add("C4", (2, 1));
+                dict.Add("C#4", (2, 2));
+                dict.Add("D4", (2, 3));
+                dict.Add("D#4", (2, 4));
+                //dict.Add("E4",(2, 5));
+                //dict.Add("F4",(2, 6));
+
+                dict.Add("E4", (1, 0));
+                dict.Add("F4", (1, 1));
+                dict.Add("F#4", (1, 2));
+                dict.Add("G4", (1, 3));
+                dict.Add("G#4", (1, 4));
+                dict.Add("A4", (1, 5));
+                dict.Add("A#4", (1, 6));
+                dict.Add("B4", (1, 7));
             }
 
-            dict.Add("E2",  (6, 0));
-            dict.Add("F2", (6, 1));
-            dict.Add("F#2", (6, 2));
-            dict.Add("G2", (6, 3));
-            dict.Add("G#2", (6, 4));
-            //dict.Add("A2", (6, 5));
-            //dict.Add("A#2", (6, 6));
-
-            dict.Add("A2",(5, 0));
-            dict.Add("A#2", (5, 1));
-            dict.Add("B2", (5, 2));
-            dict.Add("C3",(5, 3));
-            dict.Add("C#3",(5, 4));
-            //dict.Add("D3",(5, 5));
-            //dict.Add("D#3",(5, 6));
-
-            dict.Add("D3",(4, 0));
-            dict.Add("D#3",(4, 1));
-            dict.Add("E3",(4, 2));
-            dict.Add("F3",(4, 3));
-            dict.Add("F#3",(4, 4));
-            //dict.Add("G3",(4, 5));
-            //dict.Add("G#3",(4, 6));
-
-            dict.Add("G3",(3, 0));
-            dict.Add("G#3",(3, 1));
-            dict.Add("A3",(3, 2));
-            dict.Add("A#3",(3, 3));
-            //dict.Add("B3",(3, 4));
-            //dict.Add("C4",(3, 5));
-            //dict.Add("C#4",(3, 6));
-
-            dict.Add("B3",(2, 0));
-            dict.Add("C4",(2, 1));
-            dict.Add("C#4",(2, 2));
-            dict.Add("D4",(2, 3));
-            dict.Add("D#4",(2, 4));
-            //dict.Add("E4",(2, 5));
-            //dict.Add("F4",(2, 6));
-
-            dict.Add("E4",(1, 0));
-            dict.Add("F4",(1, 1));
-            dict.Add("F#4", (1, 2));
-            dict.Add("G4",(1, 3));
-            dict.Add("G#4",(1, 4));
-            dict.Add("A4",(1, 5));
-            dict.Add("A#4",(1, 6));
-            dict.Add("B4",(1, 7));
         }
+
     }
 
     //E3,F3,G3,
