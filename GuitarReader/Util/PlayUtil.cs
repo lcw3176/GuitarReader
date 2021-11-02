c88307a872cb8fb20bee9c30b0d8f80ec182ce31
joebrooks <lcw3176@naver.com>
2021-11-02 13:15:25 +0900

    타이머 코드 제거

--- GuitarReader/Util/PlayUtil.cs
+++ GuitarReader/Util/PlayUtil.cs
@@ -2,8 +2,7 @@
 using GuitarReader.Services;
 using System;
 using System.Collections.Generic;
-using System.Windows.Controls;
-using System.Windows.Threading;
+using System.Threading.Tasks;
 using Toub.Sound.Midi;
 
 namespace GuitarReader.Util
@@ -17,77 +16,56 @@ namespace GuitarReader.Util
         public event BeatEndEvent beatEndEvent;
 
         private NoteService noteService = new NoteService();
-        private DispatcherTimer timer = new DispatcherTimer();
+        private bool isRun = false;
         private List<Note> lst;
-        private int count = 0;
+        private int index = 0;
 
-        //private static PlayUtil instacne;
-
-        //public static PlayUtil GetInstacne()
-        //{
-        //    if (instacne == null)
-        //    {
-        //        instacne = new PlayUtil();
-        //    }
-
-        //    return instacne;
-        //}
-
-
-        public PlayUtil()
+        private void PlayBeat()
         {
-            timer.Interval = TimeSpan.FromSeconds(0.5);
-            timer.Tick += Timer_Tick;
-        }
+            
+            MidiPlayer.Play(new NoteOn(0, 0, lst[index].CodeStr, 127));
+            beatPlayEvent(lst[index].stringPos, lst[index].fretPos);
+            index++;
 
-        /// <summary>
-        /// 나중에 시간 차 계산으로 변경, 지금은 테스트로 타이머
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            MidiPlayer.Play(new NoteOn(0, 0, lst[count].CodeStr, 127));
-            beatPlayEvent(lst[count].stringPos, lst[count].fretPos);
-            count++;
-
-            if (count >= lst.Count)
+            if (index >= lst.Count)
             {
-                count = 0;
-                timer.Stop();
+                index = 0;
+                isRun = false;
                 beatEndEvent();
+                
             }
         }
 
         public void Play(int songId)
         {
             lst = noteService.ReadById(songId);
+            isRun = true;
 
-            timer.Start();
+            Task.Run(() =>
+            {
+                DateTime startTime = DateTime.Now;
+
+                while (isRun)
+                {
+                    if(DateTime.Now - startTime >= TimeSpan.FromSeconds(0.5))
+                    {
+                        PlayBeat();
+                        startTime = DateTime.Now;
+                    }
+                    
+                }
+            });
         }
 
         public void Pause()
         {
-            timer.Stop();
+            isRun = true;
         }
 
         public void Stop()
         {
-            count = 0;
-            timer.Stop();
+            isRun = false;
+            index = 0;
         }
-
-        //public void Open()
-        //{
-        //    MidiPlayer.OpenMidi();
-        //    MidiPlayer.Play(new ProgramChange(0, 0, GeneralMidiInstruments.SteelAcousticGuitar));
-        //}
-
-        //public void Close()
-        //{
-        //    count = 0;
-        //    timer.Stop();
-        //    MidiPlayer.CloseMidi();
-        //}
     }
 }
