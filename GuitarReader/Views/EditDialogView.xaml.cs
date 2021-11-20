b8d82daf46c684db37c0321522b868f23a567340
joebrooks <lcw3176@naver.com>
2021-11-20 20:43:54 +0900

    EditDialogView 추가

---
+++ GuitarReader/Views/EditDialogView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using GuitarReader.ViewModels;
+using System.Windows;
+
+namespace GuitarReader.Views
+{
+    /// <summary>
+    /// Interaction logic for EditDialogView.xaml
+    /// </summary>
+    public partial class EditDialogView : Window
+    {
+        public EditDialogView(int sheetId)
+        {
+            InitializeComponent();
+            this.DataContext = new EditViewModel(gridSheet, sheetId);
+        }
+    }
+}
