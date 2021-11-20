8aa02e94bb5f4ac6e6cfe26107958dd97068b3c1
joebrooks <lcw3176@naver.com>
2021-11-20 20:43:43 +0900

    EditViewModel 추가

---
+++ GuitarReader/ViewModels/EditViewModel.cs
@@ -0,0 +1,45 @@
+﻿using GuitarReader.Command;
+using GuitarReader.Models;
+using GuitarReader.Services;
+using GuitarReader.Util;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace GuitarReader.ViewModels
+{
+    internal class EditViewModel : BaseViewModel
+    {
+        public ICommand MoveBackCommand { get; set; }
+        public ICommand MoveForwardCommand { get; set; }
+
+        private DisplayUtil displayUtil;
+        private NoteService noteService = new NoteService();
+        private List<Note> noteList;
+
+        public EditViewModel(Grid gridSheet, int sheetId)
+        {
+            displayUtil = new DisplayUtil(gridSheet);
+            noteList = noteService.ReadById(sheetId);
+            MoveBackCommand = new RelayCommand(MoveBackExecuteMethod);
+            MoveForwardCommand = new RelayCommand(MoveForwardExecuteMethod);
+            initNotes();
+        }
+
+        private void initNotes()
+        {
+            displayUtil.AddTabWithOffset(noteList);
+            
+        }
+
+        private void MoveBackExecuteMethod(object obj)
+        {
+            displayUtil.MoveBack();
+        }
+
+        private void MoveForwardExecuteMethod(object obj)
+        {
+            displayUtil.MoveForward();
+        }
+    }
+}
