e22d67b32ab5caaefef35949909d00ea7aca5c24
joebrooks <lcw3176@naver.com>
2021-11-30 15:02:05 +0900

    변수 이름 변경

--- GuitarReader/ViewModels/EditViewModel.cs
+++ GuitarReader/ViewModels/EditViewModel.cs
@@ -2,7 +2,6 @@
 using GuitarReader.Models;
 using GuitarReader.Services;
 using GuitarReader.Util;
-using System;
 using System.Collections.Generic;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -14,15 +13,15 @@ namespace GuitarReader.ViewModels
         public ICommand MoveBackCommand { get; set; }
         public ICommand MoveForwardCommand { get; set; }
 
-        private DisplayUtil displayUtil;
+        private DisplayService displayService;
         private NoteService noteService = new NoteService();
         private SheetService sheetService = new SheetService();
         private List<Note> noteList;
 
         public EditViewModel(Grid gridSheet, int sheetId)
         {
-            displayUtil = new DisplayUtil(gridSheet);
-            noteList = noteService.ReadById(sheetId);
+            displayService = new DisplayService(gridSheet);
+            noteList = noteService.SelectById(sheetId);
             MoveBackCommand = new RelayCommand(MoveBackExecuteMethod);
             MoveForwardCommand = new RelayCommand(MoveForwardExecuteMethod);
             initNotes();
@@ -30,32 +29,25 @@ namespace GuitarReader.ViewModels
 
         private void initNotes()
         {
-            displayUtil.AddTabWithOffset(noteList);
+            displayService.AddTabWithOffset(noteList);
             
         }
 
         private void MoveBackExecuteMethod(object obj)
         {
-            displayUtil.MoveBack();
+            displayService.MoveBack();
         }
 
         private void MoveForwardExecuteMethod(object obj)
         {
-            displayUtil.MoveForward();
+            displayService.MoveForward();
         }
 
         public void SaveCurrentInfo()
         {
-            List<Note> temp = displayUtil.GetCurrentNotes();
+            List<Note> temp = displayService.GetCurrentNotes();
 
-            for(int i = 0; i < temp.Count; i++)
-            {
-                if(temp[i].beatLen != noteList[i].beatLen)
-                {
-                    noteService.UpdateBeatLength(noteList[i], temp[i]);
-                }
-            }
-
+            noteService.UpdateRecentData(temp);
             sheetService.UpdateRecentDate(temp[0].id);
 
         }
